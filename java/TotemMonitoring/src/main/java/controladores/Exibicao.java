79c3e4add58a6d54dc3f7ad0e2822f623de65ee5
lucas-pires <pirefrancalucas@gmail.com>
2019-11-27 20:02:35 -0300

    Alteração na lógica de alertas

--- java/TotemMonitoring/src/main/java/controladores/Exibicao.java
+++ java/TotemMonitoring/src/main/java/controladores/Exibicao.java
@@ -53,55 +53,60 @@ public class Exibicao {
     }
 
     private void mandarAlerta() {
-        
+
         String message, color, statusAlerta;
-        
+
         if (cpu > 80) {
             message = "Sua CPU esta ficando sobrecarregada";
             color = "#fe9918";
             statusAlerta = "Atencao";
             telaTotem.getLbCpu().setForeground(new Color(255, 225, 4));
-            
-            if (cpu > 90){
+
+            if (cpu > 90) {
                 message = "Sua CPU esta em risco";
                 color = "#ec0505";
                 statusAlerta = "Critico";
                 telaTotem.getLbCpu().setForeground(new Color(255, 0, 0));
             }
             alerta.enviarAlerta(message, color, statusAlerta);
+        } else {
+            telaTotem.getLbCpu().setForeground(new Color(0, 255, 0));
         }
         if (memoria > 80) {
             message = "Voce esta ficando sem armazenamento na memoria";
             color = "#fe9918";
             statusAlerta = "Atencao";
             telaTotem.getLbMemoria().setForeground(new Color(255, 225, 4));
-            
-            if (memoria < 90){
+
+            if (memoria < 90) {
                 message = "Sua memoria esta critica";
                 color = "#ec0505";
                 statusAlerta = "Critico";
                 telaTotem.getLbMemoria().setForeground(new Color(255, 0, 0));
             }
             alerta.enviarAlerta(message, color, statusAlerta);
+        } else{
+            telaTotem.getLbMemoria().setForeground(new Color(0, 255, 0));
         }
-        if (disco < 80) {
+        if (disco > 80) {
             message = "Voce esta ficando sem espaço no HD";
             color = "#fe9918";
             statusAlerta = "Atencao";
             telaTotem.getLbDisco().setForeground(new Color(255, 225, 4));
-            
-            if (disco < 97){
+
+            if (disco > 97) {
                 message = "Seu HD esta cheio";
                 color = "#ec0505";
                 statusAlerta = "Critico";
                 telaTotem.getLbDisco().setForeground(new Color(255, 0, 0));
             }
             alerta.enviarAlerta(message, color, statusAlerta);
-        }
-        else{
-            telaTotem.getLbCpu().setForeground(new Color(0, 255, 0));
-            telaTotem.getLbMemoria().setForeground(new Color(0, 255, 0));
+        } else{
             telaTotem.getLbDisco().setForeground(new Color(0, 255, 0));
-    }
+        }
+
+    
+        
+
     }
 }
